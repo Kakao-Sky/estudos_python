eb1a28d2e40ee6cafdd8af68f1239fae5df2f77f
Carroline_Bento <42155449+carroline-bento@users.noreply.github.com>
2023-02-10 15:37:16 -0300

    Add files via upload

---
+++ aula99_package/__init__.py
@@ -0,0 +1,11 @@
+# aula 159
+# isso "engana" o Python, o package vai passar a se comportar como um módulo
+"""
+print('Você importou', __name__)
+
+def dobra(x):
+    return x * 2
+
+    """
+
+from aula99_package.modulo import soma_do_modulo
