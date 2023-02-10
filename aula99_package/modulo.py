2e8b3a1f3d1c12fc37ef62f3b7ae1385dfa12799
Carroline_Bento <42155449+carroline-bento@users.noreply.github.com>
2023-02-10 15:36:44 -0300

    Update modulo.py

--- aula99_package/modulo.py
+++ aula99_package/modulo.py
@@ -1,5 +1,11 @@
-from aula99_package.modulo_b import fala_oi
+# Para importar algo dentro deste módulo é importante pensar nos lugares em que este módulo será usado
+# do ponto de vista do __main__, pode ser que algum outro módulo importado aqui não existe pra ele
+# por isso, é sempre bom importar algo do ponto de vista do __main__
+
+#from modulo_b import fala_oi # fazendo a importação sem levar em consideração o __main__
+from aula99_package.modulo_b import fala_oi # fazendo a importação levando em consideração o __main__
+
 def soma_do_modulo(x, y):
     return x + y
 
-    # tendo problemas aqui
+fala_oi()
