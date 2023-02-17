a9e92453632d44ba8673f4d5de1a73091e5ca95b
Carroline_Bento <42155449+carroline-bento@users.noreply.github.com>
2023-02-17 09:38:05 -0300

    Update aula119_exercicio.py

--- aula119_exercicio.py
+++ aula119_exercicio.py
@@ -54,3 +54,61 @@ while True:
         
 # solução do PROFESSOR
 
+def listar(tarefas):
+    print()
+    if not tarefas:
+        print('Nenhuma tarefa para listar')
+        return
+    print('Tarefas: ')
+    for tarefa in tarefas:
+        print(f'\t{tarefa}')
+
+def desfazer(tarefas, tarefas_refazer):
+    print()
+    if not tarefas:
+        print('Nenhuma tarefa para desfazer')
+        return
+    tarefa = tarefas.pop()
+    tarefas_refazer.append(tarefa)
+
+def refazer(tarefas, tarefas_refazer):
+    print()
+    if not tarefas_refazer:
+        print('Nenhuma tarefa para refazer')
+        return
+    tarefa = tarefas_refazer.pop()
+    tarefas.append(tarefa)
+
+def adicionar(tarefa, tarefas):
+    print()
+    tarefa = tarefa.strip()
+    if not tarefa:
+        print('Você não digitou nenhuma tarefa')
+        return
+    tarefas.append(tarefa)
+
+
+
+
+tarefas = []
+tarefas_refazer = []
+
+while True:
+    print('Comandos aceitos: listar, desfazer, refazer')
+    tarefa = input("Digite uma tarefa ou comando: ")
+
+    if tarefa == 'listar':
+        listar(tarefas)
+        print()
+        continue
+    elif tarefa == 'desfazer':
+        desfazer(tarefas, tarefas_refazer)
+        print()
+        continue
+    elif tarefa == 'refazer':
+        refazer(tarefas, tarefas_refazer)
+        print()
+        continue
+    else:
+        adicionar(tarefa, tarefas)
+        print()
