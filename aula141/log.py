55e473eba907909af031ddc3e1fcf3e51e7180d1
Carroline_Bento <42155449+carroline-bento@users.noreply.github.com>
2023-02-20 20:00:58 -0300

    Add files via upload

---
+++ aula141/log.py
@@ -0,0 +1,55 @@
+# Arquivo de Log
+# Abstração
+
+from pathlib import Path # Parte 3
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+class Log: 
+    # esse método log() só existe pra definir que existe o método log na classe Log
+    # padrão de projeto - Template Method
+    def _log(self, msg): # assinatura do método
+        raise NotImplementedError('Implemente o método log') # abstração -> eu não quero que 
+        # uma pessoa utilize essa classe diretamente,
+        #  eu quero que ela utilize a subclasse
+        # NotImplementedError -> significa que está usando uma classe que não deveria ser usada
+
+    def log_error(self, msg): # Parte 2
+        return self._log(f'Error: {msg}')
+        # self.log -> faz referência ao self e ao método log() da classe em que vai ser chamado
+    
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+
+class LogFileMixin(Log):
+    # mixin -> estou falando p/ outros desenvolvedores adicionarem funcionalidades na sua herança múltipla
+    # ou seja, é para adicionar coisas dentro das outras classes, não dentro da família do seu objeto 
+    # mixin -> é uma classe que tem funcionalidades que serão adicionadas em outras classes,
+    # que podem não ser dessa família
+    def _log(self, msg): # se está sobrepondo um método, assinaturas devem ser IGUAIS
+        print(msg)
+        msg_formatada = f'{msg} {self.__class__.__name__}'
+        print(f'Salvando: {msg_formatada}')
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
+
+    # log_error pode ser acessado aqui tbm, e vai chamar o mét. log daqui e o self daqui
+
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+
+
+if __name__ == '__main__':
+    lp = LogPrintMixin()
+    lp.log_error('Qualquer coisa')
+    lp.log_success('Deu bom')
+
+    lf = LogFileMixin()
+    lf.log_error('outra coisa')
+    lf.log_success('que legal')
+    # print(LOG_FILE)
---
+++ aula141/main.py
@@ -0,0 +1,13 @@
+# from log import LogFileMixin, LogPrintMixin
+from eletronico import Smartphone
+
+galaxy_s = Smartphone('Galaxy S')
+iphone = Smartphone('iPhone')
+
+
+galaxy_s.ligar()
+galaxy_s.ligar()
+iphone.desligar()
+iphone.desligar()
+iphone.ligar()
+galaxy_s.desligar()
